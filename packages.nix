80c2d213b802c499e9bbb51d0f161ad0aae26361
Ben Siraphob <bensiraphob@gmail.com>
2026-05-09 23:52:28 -0700

    gcc46: stub c-common bootstrap unit

--- packages.nix
+++ packages.nix
@@ -2543,6 +2543,7 @@ C
         patch -p1 < ${./patches/gcc46-darwin-c-objc-common-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-c-parser-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-genhooks-bootstrap.patch}
+        patch -p1 < ${./patches/gcc46-darwin-c-common-bootstrap.patch}
         cd ../..
         find work/build -type f -name Makefile -print | while read makefile; do
           sed -i \
