1f5caa873c98b403f769784c2dd7e1a9f5084642
Ben Siraphob <bensiraphob@gmail.com>
2026-05-10 15:14:34 -0700

    Revert "gcc46: stub c-typeck bootstrap unit"
    This reverts commit a114d552fc41ff1c4d2b35eea78bb19f1fdd31f3.

--- packages.nix
+++ packages.nix
@@ -2363,7 +2363,6 @@ let
       patch -p1 < ${./patches/gcc46-darwin-attribs-bootstrap.patch}
       patch -p1 < ${./patches/gcc46-darwin-c-errors-bootstrap.patch}
       patch -p1 < ${./patches/gcc46-darwin-c-decl-bootstrap.patch}
-      patch -p1 < ${./patches/gcc46-darwin-c-typeck-bootstrap.patch}
     '';
 
   phase35-gcc46-all-gcc =
