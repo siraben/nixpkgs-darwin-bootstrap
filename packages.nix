f4172dca9c8e3c7bb127a250525e80e8beef58eb
Ben Siraphob <bensiraphob@gmail.com>
2026-05-10 15:14:34 -0700

    Revert "gcc46: stub genchecksum bootstrap generator"
    This reverts commit 67839815519be40ce87a96d92d011b12ee66b0da.

--- packages.nix
+++ packages.nix
@@ -2559,7 +2559,6 @@ C
         patch -p1 < ${./patches/gcc46-darwin-c-ada-spec-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-i386-c-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-darwin-c-bootstrap.patch}
-        patch -p1 < ${./patches/gcc46-darwin-genchecksum-bootstrap.patch}
         cd ../..
         find work/build -type f -name Makefile -print | while read makefile; do
           sed -i \
