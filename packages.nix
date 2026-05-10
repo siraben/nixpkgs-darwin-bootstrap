e4d0b075d8c66fd07e11559a3c111efa83385be2
Ben Siraphob <bensiraphob@gmail.com>
2026-05-10 01:12:01 -0700

    gcc46: stub c-dump bootstrap unit

--- packages.nix
+++ packages.nix
@@ -2545,6 +2545,7 @@ C
         patch -p1 < ${./patches/gcc46-darwin-genhooks-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-c-common-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-c-cppbuiltin-bootstrap.patch}
+        patch -p1 < ${./patches/gcc46-darwin-c-dump-bootstrap.patch}
         cd ../..
         find work/build -type f -name Makefile -print | while read makefile; do
           sed -i \
