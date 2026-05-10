09fde5509f18c97f2b6679ad783460d20c4c1549
Ben Siraphob <bensiraphob@gmail.com>
2026-05-10 15:14:34 -0700

    Revert "gcc46: stub cfg bootstrap unit"
    This reverts commit 2f237c040ff7396b90cebd3a0c88beee6a7dd1bf.

--- packages.nix
+++ packages.nix
@@ -2566,7 +2566,6 @@ C
         patch -p1 < ${./patches/gcc46-darwin-builtins-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-caller-save-bootstrap.patch}
         patch -p1 < ${./patches/gcc46-darwin-calls-bootstrap.patch}
-        patch -p1 < ${./patches/gcc46-darwin-cfg-bootstrap.patch}
         cd ../..
         find work/build -type f -name Makefile -print | while read makefile; do
           sed -i \
