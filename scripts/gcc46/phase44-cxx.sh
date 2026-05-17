7882e732be6f9e5088cb328d2423e46aeeb69f92
Ben Siraphob <bensiraphob@gmail.com>
2026-05-17 03:04:55 -0700

    bootstrap: filter imported autoconf environment cache

--- scripts/gcc46/phase44-cxx.sh
+++ scripts/gcc46/phase44-cxx.sh
@@ -40,7 +40,9 @@ cd build
 for cache_dir in gcc libiberty build-x86_64-apple-darwin/libiberty mpfr mpc; do
   if [ -f "$phase35/share/darwin-bootstrap/work/build/$cache_dir/config.cache" ]; then
     mkdir -p "$cache_dir"
-    cp "$phase35/share/darwin-bootstrap/work/build/$cache_dir/config.cache" "$cache_dir/config.cache"
+    grep -v '^ac_cv_env_' \
+      "$phase35/share/darwin-bootstrap/work/build/$cache_dir/config.cache" \
+      > "$cache_dir/config.cache"
     chmod u+w "$cache_dir/config.cache"
   fi
 done
