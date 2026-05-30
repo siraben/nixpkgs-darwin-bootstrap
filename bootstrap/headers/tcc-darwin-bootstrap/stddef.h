e6ca8605ea084f3fdacb6c424b316f7868222e7d
Ben Siraphob <bensiraphob@gmail.com>
2026-05-30 05:53:31 -0700

    bake: define wint_t in bootstrap stddef.h (mpfr vasprintf needs it)

--- bootstrap/headers/tcc-darwin-bootstrap/stddef.h
+++ bootstrap/headers/tcc-darwin-bootstrap/stddef.h
@@ -8,6 +8,10 @@ typedef unsigned long uintptr_t;
 #ifndef __cplusplus
 typedef int wchar_t;
 #endif
+#ifndef _WINT_T
+#define _WINT_T
+typedef int wint_t;   /* Darwin: __darwin_wint_t == int; needed by mpfr vasprintf */
+#endif
 #ifndef NULL
 #ifdef __cplusplus
 #define NULL 0
