4510ab9d0059394f841fe78a4ef47f809fab0d09
Ben Siraphob <bensiraphob@gmail.com>
2026-05-30 09:40:39 -0700

    bake: getopt.h extern "C" guard (gcc-10 C++ gen tools link libiberty getopt_long)

--- bootstrap/headers/tcc-darwin-bootstrap/getopt.h
+++ bootstrap/headers/tcc-darwin-bootstrap/getopt.h
@@ -1,5 +1,8 @@
 #ifndef _DARWIN_BOOTSTRAP_GETOPT_H
 #define _DARWIN_BOOTSTRAP_GETOPT_H
+#ifdef __cplusplus
+extern "C" {
+#endif
 #define no_argument 0
 #define required_argument 1
 #define optional_argument 2
@@ -8,6 +11,11 @@ extern char *optarg;
 extern int optind;
 extern int opterr;
 extern int optopt;
+/* extern "C" so gcc-10's C++ build tools (gengtype etc.) reference the
+ * unmangled getopt/getopt_long that libiberty defines in C. */
 int getopt(int, char * const *, const char *);
 int getopt_long(int, char * const *, const char *, const struct option *, int *);
+#ifdef __cplusplus
+}
+#endif
 #endif
